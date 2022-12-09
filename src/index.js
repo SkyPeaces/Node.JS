ad25a9ab3f687fdb7fa9ed15a6746267e7eb047f
SkyPeaces <73152421+SkyPeaces@users.noreply.github.com>
2022-12-09 15:05:53 +0700

    hom nay nghi mot bua

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const app = express();
 const port = process.env.PORT;
 
 const corsOptions = {
-  origin: "http://localhost:3000",
-  credentials: true, //access-control-allow-credentials:true
-  optionSuccessStatus: 200,
+    origin: process.env.URL_REACT,
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
 };
 app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
@@ -24,5 +24,5 @@ initWebRoute(app);
 connectDB();
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
