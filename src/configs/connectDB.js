fdb88e0a3cdd6542cab2ec28ac4fdfd511d8e367
SkyPeaces <73152421+SkyPeaces@users.noreply.github.com>
2022-12-10 21:13:33 +0700

    config db

--- src/configs/connectDB.js
+++ src/configs/connectDB.js
@@ -1,35 +1,3 @@
-// import Sequelize from "sequelize";
-
-// // Option 3: Passing parameters separately (other dialects)
-// const sequelize = new Sequelize("database", "username", "password", {
-//   host: "localhost",
-//   dialect: "mysql",
-// });
-// let connection = require("mysql2-promise")();
-
-// // async function main() {
-// //   try {
-// // create the connection
-// connection.configure({
-//   host: "localhost",
-//   user: "root",
-//   database: "node.js",
-// });
-// //     console.log("Successfully connected to mySQL Database");
-// //   } catch (err) {
-// //     console.error(err);
-// //   } finally {
-// //     if (connection) {
-// //       try {
-// //         await connection.close();
-// //       } catch (err) {
-// //         console.error(err);
-// //       }
-// //     }
-// //   }
-// // }
-
-// export default connection;
 const { Sequelize } = require("sequelize");
 require("dotenv").config();
 const env = process.env.NODE_ENV || "development";
@@ -42,11 +10,6 @@ if (config.use_env_variable) {
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
-// const sequelize = new Sequelize("khaxqjrl_root", "khaxqjrl_hoant", "", {
-//   host: "103.97.126.21",
-//   dialect: "mysql",
-//   logging: false,
-// });
 
 let connectDB = async () => {
   try {
