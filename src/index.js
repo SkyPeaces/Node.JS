bebb80eca38932f577a6188123e904764acbba5f
SkyPeaces <73152421+SkyPeaces@users.noreply.github.com>
2022-12-22 16:28:23 +0700

    2022/12/22 8

--- src/index.js
+++ src/index.js
@@ -9,12 +9,17 @@ require("dotenv").config();
 const app = express();
 const port = process.env.PORT;
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 // const corsOptions = {
-//     origin: process.env.URL_REACT,
+//     origin: false, //process.env.URL_REACT,
 //     credentials: true, //access-control-allow-credentials:true
 //     optionSuccessStatus: 200,
 // };
-app.use(cors());
+// app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
