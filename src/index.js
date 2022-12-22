26f4cd74a62b2bdada8456fc95bb606ea09b8926
SkyPeaces <73152421+SkyPeaces@users.noreply.github.com>
2022-12-22 15:51:34 +0700

    2022/12/22 7

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT;
 //     credentials: true, //access-control-allow-credentials:true
 //     optionSuccessStatus: 200,
 // };
-// app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
