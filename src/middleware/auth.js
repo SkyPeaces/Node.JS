1d0fed1cd093b1f3fa0f15bd32bf26ebed0d0b90
SkyPeaces <73152421+SkyPeaces@users.noreply.github.com>
2022-12-26 16:10:26 +0700

    complete jwt

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -8,7 +8,7 @@ require("dotenv").config();
 const config = process.env;
 
 const verifyToken = async (req, res, next) => {
-    const token = req.body.token || req.query.token || req.headers["authorization"];
+    const token = req.body.token || req.query.token || req.headers["x-access-token"];
 
     if (!token) {
         return res.status(403).send("A token is required for authentication");
